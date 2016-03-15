912ae07114860b7dc3a33b7555700e0a4dbf589c
jamtot <jongrammor@gmail.com>
2016-03-15 02:51:09 +0000

    Refactored the code for getting the distance to work for numbers larger than 360 degrees.

--- angles.py
+++ angles.py
@@ -8,28 +8,42 @@ import math
 # calculate and return the distance unit2 
 # needs to move to reach unit1
 def get_distance(unit1, unit2):
-    phi = abs(unit2-unit1) % 360
-    sign = 1
+    phi = (unit2-unit1) % 360
+    sign = -1
     # used to calculate sign
-    if not ((unit1-unit2 >= 0 and unit1-unit2 <= 180) or (
-            unit1-unit2 <= -180 and unit1-unit2 >= -360)):
-        sign = -1
+    if not ((phi >= 0 and phi <= 180) or (
+            phi <= -180 and phi >= -360)):
+        sign = 1
     if phi > 180:
         result = 360-phi
     else:
         result = phi
+    return (result*sign), sign
 
-    return result*sign
+def get_sign(unit1, unit2):
+    return not (unit1-unit2 >= 0 and unit1-unit2 <= 180) or (
+            unit1-unit2 <= -180 and unit1-unit2 >= -360)
+
+def new_position(unit1, unit2, variance = 20):
+    distance_to_move, sign = get_distance(unit1, unit2)
+    variance*=sign
+    # %360 to keep it whithin the circle
+    return (unit2+distance_to_move-variance)%360
 
 # distance unit2 needs to move to reach unit1
-print get_distance(90,45)  # output 45 
-print get_distance(270, 350) # output -80 
-print get_distance(350, 30) # output -40 (unit2 moves -40 degrees)   
-print get_distance(30, 350) # output 40  
+assert (get_distance(90,45) == (45, 1))
+assert (get_distance(270, 350) == (-80, -1))
+assert (get_distance(350, 30) == (-40, -1))   
+assert (get_distance(30, 350) == (40, 1))
+assert (get_distance(50, 360*4) == (50, 1))
+assert (get_distance(360*4, 50) == (-50, -1))
+print "----------"
+assert (new_position(90,45) == 70)
+assert (new_position(270, 350) == 290) 
+assert (new_position(350, 30) == 10)
+assert (new_position(30, 350) == 10)
+assert (new_position(50, 360*4) == 30)
+assert (new_position(360*4, 50) == 20)
 
-unit1 = 30
-unit2 = 350
-# then calculate distance to move by taking 20 from distance
-distance_to_move = (get_distance(unit1, unit2) - 20)
-
-print (unit2 + distance_to_move)%360 # new position 10
+print (get_distance(1, 360*4))
+print (get_distance(360*4, 1))
